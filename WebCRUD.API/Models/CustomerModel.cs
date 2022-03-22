44c44e1961520835fb2b78bf697ac221f1782e7c
Danko Nikšić <niksic.danko@gmail.com>
2022-03-22 11:22:17 +0100

    Added properties to CustomerModel

--- WebCRUD.API/Models/CustomerModel.cs
+++ WebCRUD.API/Models/CustomerModel.cs
@@ -9,6 +9,8 @@ namespace WebCRUD.API.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
     }
 }
